59e3bf1a0f49a800a0c373a7a01868e7fb1c1309
AndiniDwiPutriBasril <167855200+AndiniDwiPutriBasril@users.noreply.github.com>
2024-05-21 14:32:30 +0700

    Add files via upload

---
+++ HelloWord.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo_2301083011;
+
+/**
+ *
+ * @author ACER
+ */
+public class HelloWord {
+     public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Andini Dwiputri Basril");
+    }
+}
